ad9e403d6e3d06bb46e182af5d7ccc9166563c58
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-10 08:13:18 +0700

    update booking result page

--- src/components/pages/bookingResult.jsx
+++ src/components/pages/bookingResult.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "../../config/axios";
+import "../styles/bookingResult.css";
 
 function bookingResult() {
   const { booking_id } = useParams();
@@ -25,9 +26,24 @@ function bookingResult() {
     }
   };
   return (
-    <div>
-      <h1>bookingResult</h1>
-      <h2>bookingID : {booking_id}</h2>
+    <div className="booking-result">
+      <h1>Congratulation!</h1>
+      <span>
+        Your booking is complete. If you need any further assistance or have
+        questions about your reservation, feel free to ask or contact us. Enjoy
+        your experience!
+      </span>
+      <h2>Your booking ID: {booking_id} </h2>
+      {result.map((result) => (
+        <div key={result.id}>
+          <p>สนาม: {result.arena_id}</p>
+          <p>วันที่: {result.date}</p>
+          <p>
+            เวลา: {result.time_start} - {result.time_end}
+          </p>
+          <p>ราคา: {result.total_price}</p>
+        </div>
+      ))}
     </div>
   );
 }
